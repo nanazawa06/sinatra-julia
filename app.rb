33f21dcec9ef595d4dd454244e9ab87885cbcfe3
nanazawa06 <nanazawa06@gmail.com>
2024-01-13 21:21:31 +0900

    ジュリア集合の計算

--- app.rb
+++ app.rb
@@ -2,9 +2,24 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
-require './calcurate_julia'
+require './julia'
+require 'json'
 
 get '/' do
-  @number = Bar.new.len('abcds')
   erb :index
+end
+
+get '/satori/julia' do
+  min_x = params[:minx].to_f
+  max_x = params[:maxx].to_f
+  min_y = params[:miny].to_f
+  max_y = params[:maxy].to_f
+  comp_str = params[:complex]
+  complex = Complex(comp_str)
+  real_part = complex.real
+  imag_part = complex.imag
+
+  julia_colors = Julia.new.calculate(min_x, max_x, min_y, max_y, real_part, imag_part)
+
+  julia_colors.to_json
 end
--- extconf.rb
+++ extconf.rb
@@ -1,2 +1,2 @@
 require 'mkmf'
-create_makefile("calcurate_julia")
+create_makefile("julia")
